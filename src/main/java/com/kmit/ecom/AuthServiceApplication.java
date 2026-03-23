8497f111c8d50077e676d83754c6d3d368611d6d
maru-sumit <developer.sumit@outlook.com>
2026-03-24 00:34:25 +0530

    Move key from class to yml file

--- src/main/java/com/kmit/ecom/AuthServiceApplication.java
+++ src/main/java/com/kmit/ecom/AuthServiceApplication.java
@@ -2,8 +2,11 @@ package com.kmit.ecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude =  {UserDetailsServiceAutoConfiguration.class}
+)
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/kmit/ecom/security/JwtUtil.java
+++ src/main/java/com/kmit/ecom/security/JwtUtil.java
@@ -1,21 +1,29 @@
 package com.kmit.ecom.security;
 
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private final String SECRET = "kbrSoSj7Zob1iikAilhfiQsPjQMdjnCP6sIo2lOQilc=";
+    @Value("${jwt.secret}")
+    private String secret;
+
+//    private final Key key = Keys.hmacShaKeyFor(secret.getBytes());
 
     public String generateToken(String username) {
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .signWith(key)
                 .compact();
     }
 }
